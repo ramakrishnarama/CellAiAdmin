bf6bb464615e5f4f0f87396d1b32b522515de896
ramakrishnarama <ramakrishna.m173@gmail.com>
2025-07-05 20:16:46 +0530

    Type error

--- src/app/(admin)/(others-pages)/(chart)/line-chart/page.tsx
+++ src/app/(admin)/(others-pages)/(chart)/line-chart/page.tsx
@@ -1,5 +1,5 @@
-import LineChartOne from "@/components/charts/line/LineChartOne";
-import ComponentCard from "@/components/common/ComponentCard";
+// import LineChartOne from "@/components/charts/line/LineChartOne";
+// import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { Metadata } from "next";
 import React from "react";
@@ -14,9 +14,9 @@ export default function LineChart() {
     <div>
       <PageBreadcrumb pageTitle="Line Chart" />
       <div className="space-y-6">
-        <ComponentCard title="Line Chart 1">
-          <LineChartOne />
-        </ComponentCard>
+        {/* <ComponentCard title="Line Chart 1"> */}
+          {/* <LineChartOne /> */}
+        {/* </ComponentCard> */}
       </div>
     </div>
   );
