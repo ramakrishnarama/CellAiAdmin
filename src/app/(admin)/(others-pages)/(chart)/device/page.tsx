4da353ab22a48d65403211e7720c652a9b665ffa
ramakrishnarama <ramakrishna.m173@gmail.com>
2025-07-29 21:59:04 +0530

    new battery data added

--- src/app/(admin)/(others-pages)/(chart)/device/page.tsx
+++ src/app/(admin)/(others-pages)/(chart)/device/page.tsx
@@ -1,6 +1,13 @@
+// app/device/page.tsx
 "use client";
+
+import { Suspense } from "react";
 import DevicePage from "@/components/device/DevicePage";
 
 export default function Page() {
-  return <DevicePage />;
+  return (
+    <Suspense fallback={<div className="text-white">Loading device page...</div>}>
+      <DevicePage />
+    </Suspense>
+  );
 }
